845c38b3fb30596e9cd70a2a793a8a8b1531fb79
Dan Miller <dan.r.miller@gmail.com>
2018-05-07 09:58:59 -0700

    don't copy shared files

--- windeploy_helper.sh
+++ windeploy_helper.sh
@@ -23,17 +23,17 @@ else
 	ICU_FILES=(libicudtd61.dll libicuind61.dll libicuiod61.dll libicutud61.dll libicuucd61.dll)
 fi
 
-FILES=(zlib1.dll libwinpthread-1.dll libtiff-5.dll libstdc++-6.dll libpng16-16.dll libpcre16-0.dll libpcre-1.dll libmng-2.dll liblzma-5.dll liblcms2-2.dll libjpeg-8.dll libintl-8.dll libiconv-2.dll libharfbuzz-0.dll libgraphite2.dll libglib-2.0-0.dll libfreetype-6.dll libbz2-1.dll libssp-0.dll libpcre2-16-0.dll)
+#FILES=(zlib1.dll libwinpthread-1.dll libtiff-5.dll libstdc++-6.dll libpng16-16.dll libpcre16-0.dll libpcre-1.dll libmng-2.dll liblzma-5.dll liblcms2-2.dll libjpeg-8.dll libintl-8.dll libiconv-2.dll libharfbuzz-0.dll libgraphite2.dll libglib-2.0-0.dll libfreetype-6.dll libbz2-1.dll libssp-0.dll libpcre2-16-0.dll)
 
-platform=$(get_platform)
+#platform=$(get_platform)
 
-if [[ "$platform" == "mingw64" ]]; then
-	PLATFORM_FILES="libgcc_s_seh-1.dll" 
-elif [[ "$platform" == "mingw32" ]]; then 
-	PLATFORM_FILES="libgcc_s_dw2-1.dll" 
-fi
+#if [[ "$platform" == "mingw64" ]]; then
+#	PLATFORM_FILES="libgcc_s_seh-1.dll" 
+#elif [[ "$platform" == "mingw32" ]]; then 
+#	PLATFORM_FILES="libgcc_s_dw2-1.dll" 
+#fi
 
-for f in "${FILES[@]}"; do cp $MSYSTEM_PREFIX/bin/$f $TARGET || exit 1; done
+#for f in "${FILES[@]}"; do cp $MSYSTEM_PREFIX/bin/$f $TARGET || exit 1; done
 
 for f in "${ICU_FILES[@]}"; do cp $MSYSTEM_PREFIX/bin/$f $TARGET || exit 1; done
 
